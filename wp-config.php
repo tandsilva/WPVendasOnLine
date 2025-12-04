91ec55d1dcbb6fa48067e19a9289c231bf2f167d
Aldrin Marques <tamdsilva@logatti.edu.br>
2025-12-04 14:07:18 -0300

    Aajuste

--- wp-config.php
+++ wp-config.php
@@ -1,25 +1,32 @@
 <?php
 
-define('DB_NAME', getenv('MYSQL_DATABASE'));
-define('DB_USER', getenv('MYSQL_USER'));
-define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
-define('DB_HOST', getenv('MYSQL_HOST'));
+define( 'DB_NAME', 'railway' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'xofVyskEDcfZunTcWInESoltfnnXBMLx' );
+define( 'DB_HOST', 'mysql.railway.internal:3306' );
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_COLLATE', '' );
 
-define('DB_CHARSET', 'utf8mb4');
-define('DB_COLLATE', '');
-
-define('AUTH_KEY',         'substitua-com-chaves-geradas');
-define('SECURE_AUTH_KEY',  'substitua-com-chaves-geradas');
-define('LOGGED_IN_KEY',    'substitua-com-chaves-geradas');
-define('NONCE_KEY',        'substitua-com-chaves-geradas');
-define('AUTH_SALT',        'substitua-com-chaves-geradas');
-define('SECURE_AUTH_SALT', 'substitua-com-chaves-geradas');
-define('LOGGED_IN_SALT',   'substitua-com-chaves-geradas');
-define('NONCE_SALT',       'substitua-com-chaves-geradas');
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
 $table_prefix = 'wp_';
-define('WP_DEBUG', false);
-define('WP_ALLOW_REPAIR', true);
 
-if (!defined('ABSPATH')) define('ABSPATH', __DIR__ . '/');
+define( 'WP_DEBUG', false );
+
+/* Repair */
+define( 'WP_ALLOW_REPAIR', true );
+
+/* That's all, stop editing! Happy publishing. */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+
 require_once ABSPATH . 'wp-settings.php';
